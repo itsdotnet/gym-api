2757f4f7b2c11c058ab946058299b068f027c5f5
Iskandar Kodirov <eiscocyber@gmail.com>
2024-05-27 17:18:26 +0500

    Extensions added

---
+++ src/Gym.Service/Extensions/CollectionExtensions.cs
@@ -0,0 +1,13 @@
+using Gym.Domain.Configurations;
+
+namespace Gym.Service.Extensions;
+
+
+public static class CollectionExtension
+{
+    public static IQueryable<T> ToPaginate<T>(this IQueryable<T> values, PaginationParams @params)
+    {
+        var source = values.Skip((@params.PageIndex - 1) * @params.PageSize).Take(@params.PageSize);
+        return source;
+    }
+}
