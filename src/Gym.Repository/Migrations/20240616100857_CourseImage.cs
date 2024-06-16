49dfd9a13fb35b62dbee15ffcf5003d8932d1ce8
Iskandar Kodirov <eiscocyber@gmail.com>
2024-06-16 15:16:19 +0500

    New property for course

---
+++ src/Gym.Repository/Migrations/20240616100857_CourseImage.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gym.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class CourseImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "AttachmentId",
+                table: "Courses",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_AttachmentId",
+                table: "Courses",
+                column: "AttachmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_Attachments_AttachmentId",
+                table: "Courses",
+                column: "AttachmentId",
+                principalTable: "Attachments",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_Attachments_AttachmentId",
+                table: "Courses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_AttachmentId",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "AttachmentId",
+                table: "Courses");
+        }
+    }
+}
